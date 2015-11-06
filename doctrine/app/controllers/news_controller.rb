95d9ee8cbaf1673a9e7744dbccb7b191c312f09d
yavnodm@gmail.com <yavnodm@gmail.com>
2015-11-07 01:03:52 +0300

    markup for the first page

--- doctrine/app/controllers/news_controller.rb
+++ doctrine/app/controllers/news_controller.rb
@@ -1,7 +1,13 @@
 class NewsController < ApplicationController
 
-  def index
+  before_action :set_body_class
 
+  def set_body_class
+    @body_class = :bluegamma
+  end
+
+  def index
+    render :layout => true
   end
 
 end
